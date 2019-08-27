f8022462a60c52efa213c6a35f858561f0dc0db0
kel29 <krista.loven.87@gmail.com>
2019-08-27 14:42:46 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.slice(-1) == "."
   end
 
   def question?
-
+    self.slice(-1) == "?"
   end
 
   def exclamation?
-
+    self.slice(-1) == "!"
   end
 
   def count_sentences
-
+    arr = self.split(".").map { |i| i.split("?") }.flatten.map { |i| i.split("!") }.flatten
+    length = arr.length
+    arr.each { |i| length -= 1 if i == "" } 
+    length
   end
 end
