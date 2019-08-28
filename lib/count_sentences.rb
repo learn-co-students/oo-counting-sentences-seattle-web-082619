8e3c09272e26e4fc1a1d0459de90bb5aa7d5b62c
github username <github email address>
2019-08-28 09:00:13 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,35 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true 
+    else 
+      return false 
+    end 
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true 
+    else 
+      return false 
+    end 
   end
 
   def exclamation?
-
+    if self.end_with?("!") 
+      return true 
+    else 
+      return false 
+    end 
   end
 
   def count_sentences
+    #edge case 
+    # if self.count_sentences == 0 
+    #   return 0 
+    # end #end of if 
 
   end
+
 end
